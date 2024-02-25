5eeaede7becb17160a752dc0e82390aa4bfae752
imMatheus <matheusmendesbarata03@gmail.com>
2024-02-25 22:39:16 +0100

    Refactor header component and remove unused code

---
+++ src/content/blog/abc/Count.svelte
@@ -0,0 +1,12 @@
+<!-- simple counter component -->
+<script>
+  let count = 0
+
+  function handleClick() {
+    count += 1
+  }
+</script>
+
+<div class="p-5">
+  <button class="p-4 bg-red-500" on:click={handleClick}>{count}</button>
+</div>
